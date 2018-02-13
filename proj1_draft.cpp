71ce10ef8382c5d17e110942b723385dd2985f11
firen777 <hivemindac1995@gmail.com>
2018-02-13 11:45:55 -0800

    mid way draft

--- proj1_draft.cpp
+++ proj1_draft.cpp
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
+#include <cstdio>
 #ifdef __APPLE__
   #include <OpenGL/gl.h>
   #include <OpenGL/glu.h>
@@ -26,6 +27,9 @@
 #define NORTHWEST 7
 #define TRUE 1
 #define FALSE 0
+
+#define SQUARE_X 80
+#define SQUARE_Y 60
 //=======Struct=======//
 /**int r,g,b
 */
@@ -54,7 +58,7 @@ typedef struct ball_type
   int radius;
   ball_color color;
 } ball_type;
-//=======Ball Instantiate=======//
+//=======Global Var=======//
 ball_type ball1;
 ball_type ball2;
 ball_type ball3;
@@ -64,18 +68,44 @@ ball_type ball6;
 ball_type ball7;
 ball_type ball8;
 
+//data matrix for marching square corners. Float value is for Linear Interpolation
+float dat_mat [SQUARE_X+1][SQUARE_Y+1]; 
+//global control for Linear Interpolation, Plain Circle 
+bool linear_interp, plain_circle;
 //=======Func Proto=======//
-int ball_hit_wall (ball_type);
-int select_ball_direction (ball_type);
-ball_type move_ball (ball_type);
-void display (void);
-void reshape (int, int);
-void keyboard (unsigned char, int, int);
-
 /**Determine whether the ball hit the wall
  * @param ball_type The ball to be checked
  * @return TRUE if hit; FALSE if not hit. NOTE: TRUE, FALSE defined as 1 and 0
 */
+int ball_hit_wall (ball_type);
+/**Generate a new direction with a given ball
+ * @param ball_type ball to be analyzed
+ * @return direction
+*/
+int select_ball_direction (ball_type);
+/**Move the ball position by 1 on X axis or Y axis or both, depending on the ball direction
+ * @param ball_type ball to be moved
+ * @return new ball data.
+*/
+ball_type move_ball (ball_type);
+/**The draw function for balls
+ * @param ball_type ball to be drawn
+*/
+void draw_ball (ball_type ball);
+/**The draw function for metaballs. Using global variables of balls
+ * 
+*/
+void draw_meta ();
+/**Calculate Marching squares corners value. Using global variables of dat_mat
+ * 
+*/
+void calc_dat_mat();
+
+void display (void);
+void reshape (int, int);
+void keyboard (unsigned char, int, int);
+
+//=======Func Implementations=======//
 int ball_hit_wall (ball_type ball)
 {
   int ball_hit_wall;
@@ -136,11 +166,6 @@ int ball_hit_wall (ball_type ball)
   }
   return ball_hit_wall;
 }
-
-/**Generate a new direction with a given ball
- * @param ball_type ball to be analyzed
- * @return direction
-*/
 int select_ball_direction (ball_type ball)
 {
   int new_ball_direction, random_direction;
@@ -429,11 +454,6 @@ int select_ball_direction (ball_type ball)
   ball.position.direction = new_ball_direction;
   return ball.position.direction;
 }
-
-/**Move the ball position by 1 on X axis or Y axis or both, depending on the ball direction
- * @param ball_type ball to be moved
- * @return new ball data.
-*/
 ball_type move_ball (ball_type ball)
 {
   ball_type new_ball;
@@ -474,16 +494,12 @@ ball_type move_ball (ball_type ball)
   }
   return new_ball;
 }
-
-/**The draw function for balls
- * @param ball_type ball to be drawn
-*/
 void draw_ball (ball_type ball)
 {
   float theta, circle_iterations = 12.0;
 
   glColor3ub (ball.color.r, ball.color.g, ball.color.b);
-  glPolygonMode (GL_FRONT, GL_FILL);
+  glPolygonMode (GL_FRONT_AND_BACK, GL_LINE);
   glBegin (GL_POLYGON);
   glVertex3f (ball.position.x + ball.radius, ball.position.y, 0.0f);
   for (theta = 0; theta < 2 * M_PI; theta += M_PI / circle_iterations)
@@ -492,22 +508,77 @@ void draw_ball (ball_type ball)
   }
   glEnd();
 }
-
-/**
- * 
-*/
 void draw_meta ()
 {
-  draw_ball (ball1);
-  draw_ball (ball2);
-  draw_ball (ball3);
-  draw_ball (ball4);
-  draw_ball (ball5);
-  draw_ball (ball6);
-  draw_ball (ball7);
-  draw_ball (ball8);
-}
+  glColor3ub (0, 0xff, 0);
+  glPolygonMode (GL_FRONT_AND_BACK, GL_FILL);
+  if (linear_interp)
+  {
+    //TODO: implement Linear Interpolation if have time
+  }
+  else
+  {
+    for (int i=0; i<SQUARE_X; i++)
+      for (int j=0; j<SQUARE_Y; j++)
+      {
+        float xMid = ((float)i+0.5)*(float)X_RESOLUTION/(float)SQUARE_X; //X center of the square
+        float yMid = ((float)i+0.5)*(float)Y_RESOLUTION/(float)SQUARE_Y; //Y center of the square
+        unsigned char sqr = 0;
+        sqr | (dat_mat[i][j]>=1.0    ? 1:0);
+        sqr | (dat_mat[i+1][j]>=1.0  ? 2:0);
+        sqr | (dat_mat[i][j+1]>=1.0  ? 4:0);
+        sqr | (dat_mat[i+1][j+1]>=1.0? 8:0);
+        glBegin(GL_POLYGON);
+        switch(sqr)
+        {
+          case 0x0:
+            break;
+          case 0x1:
+            //glVertex3f (ball.position.x + ball.radius, ball.position.y, 0.0f);
+        }
+        glEnd();
 
+      }
+  }
+}
+void calc_dat_mat()
+{
+  for (int i=0; i<SQUARE_X+1; i++)
+    for (int j=0; j<SQUARE_Y+1; j++)
+    {
+      float tempVar = 0.0; //temp Answer to be stored in matrix
+      float tempX = (float)i*(float)X_RESOLUTION/(float)SQUARE_X; //X coordinate of the corner to be checked
+      float tempY = (float)j*(float)Y_RESOLUTION/(float)SQUARE_Y; //X coordinate of the corner to be checked
+      
+      tempVar += (float)(ball1.radius*ball1.radius)/
+                 ((tempX-(float)ball1.position.x)*(tempX-(float)ball1.position.x)+
+                 (tempY-(float)ball1.position.y)*(tempY-(float)ball1.position.y));
+      tempVar += (float)(ball2.radius*ball2.radius)/
+                 ((tempX-(float)ball2.position.x)*(tempX-(float)ball2.position.x)+
+                 (tempY-(float)ball2.position.y)*(tempY-(float)ball2.position.y));
+      tempVar += (float)(ball3.radius*ball3.radius)/
+                 ((tempX-(float)ball3.position.x)*(tempX-(float)ball3.position.x)+
+                 (tempY-(float)ball3.position.y)*(tempY-(float)ball3.position.y));
+      tempVar += (float)(ball4.radius*ball4.radius)/
+                 ((tempX-(float)ball4.position.x)*(tempX-(float)ball4.position.x)+
+                 (tempY-(float)ball4.position.y)*(tempY-(float)ball4.position.y));
+      tempVar += (float)(ball5.radius*ball5.radius)/
+                 ((tempX-(float)ball5.position.x)*(tempX-(float)ball5.position.x)+
+                 (tempY-(float)ball5.position.y)*(tempY-(float)ball5.position.y));
+      tempVar += (float)(ball6.radius*ball6.radius)/
+                 ((tempX-(float)ball6.position.x)*(tempX-(float)ball6.position.x)+
+                 (tempY-(float)ball6.position.y)*(tempY-(float)ball6.position.y));
+      tempVar += (float)(ball7.radius*ball7.radius)/
+                 ((tempX-(float)ball7.position.x)*(tempX-(float)ball7.position.x)+
+                 (tempY-(float)ball7.position.y)*(tempY-(float)ball7.position.y));
+      tempVar += (float)(ball8.radius*ball8.radius)/
+                 ((tempX-(float)ball8.position.x)*(tempX-(float)ball8.position.x)+
+                 (tempY-(float)ball8.position.y)*(tempY-(float)ball8.position.y));
+      
+      dat_mat[i][j]=tempVar;
+    }
+  //printf("%f\n",dat_mat[SQUARE_X/2][SQUARE_Y/2]);
+}
 void display(void)
 {
   // * Ball 1 *
@@ -561,14 +632,27 @@ void display(void)
 
   // clear the screen to black
   glColor3ub(0, 0, 0);
-  glPolygonMode(GL_FRONT, GL_FILL);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
   glRecti(0, 0, X_RESOLUTION, Y_RESOLUTION);
-  
+
+  calc_dat_mat();
   draw_meta();
+
+  if (plain_circle)
+  {
+    draw_ball (ball1);
+    draw_ball (ball2);
+    draw_ball (ball3);
+    draw_ball (ball4);
+    draw_ball (ball5);
+    draw_ball (ball6);
+    draw_ball (ball7);
+    draw_ball (ball8);
+  }
+
   glutSwapBuffers();
   glutPostRedisplay();
 }
-
 void reshape (int w, int h)  
 {
   glViewport (0, 0, w, h);
@@ -585,7 +669,6 @@ void reshape (int w, int h)
   glMatrixMode(GL_MODELVIEW);  
   glLoadIdentity(); 
 }
-
 void keyboard (unsigned char key, int x, int y) 
 {
   switch (key) 
@@ -691,6 +774,11 @@ int main (int argc, char *argv[])
   //
   // seed the random number generator
   srandom (time(0));
+  // initialize control for drawing plain circle
+  plain_circle = true;
+  // initialize control for using Linear Interpolation
+  linear_interp = false;
+
   glutInit (&argc, argv);
   glutInitDisplayMode (GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); 
   glutInitWindowSize (X_RESOLUTION, Y_RESOLUTION);
