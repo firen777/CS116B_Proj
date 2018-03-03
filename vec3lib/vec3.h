4fc00f541b1f48b0f3018fb346c6d5a8c5a29fdc
firen777 <hivemindac1995@gmail.com>
2018-03-03 02:08:12 -0800

    trying out custom vec3 library

---
+++ vec3lib/vec3.h
@@ -0,0 +1,33 @@
+/**Author: Un Hou (Albert) Chan
+ * 
+*/
+
+/**
+ * 3D Vector, floating point version
+*/
+class Vec3f
+{
+    private:
+        float x;
+        float y;
+        float z;
+        float l;
+    public:
+        Vec3f(float _x, float _y, float _z, float _l);
+
+};
+
+/**
+ * 3D Vector, integer point version
+*/
+class Vec3i
+{
+    private:
+        int x;
+        int y;
+        int z;
+        int l;
+    public:
+
+    
+};
