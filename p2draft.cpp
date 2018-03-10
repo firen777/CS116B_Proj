c7c2f1e01fa3fcb7a1af76204ac21f6df479c92d
firen777 <hivemindac1995@gmail.com>
2018-03-10 01:47:39 -0800

    color and stuff

--- p2draft.cpp
+++ p2draft.cpp
@@ -10,25 +10,10 @@
   #include <GL/glut.h> 
 #endif
 
-
-
-
 int   cube_exploded = 0;
 float angle = 0.0; // camera rotation angle
 
-
-//
 // Light sources
-GLfloat  light0Amb[4] =  { 1.0, 0.6, 0.2, 1.0 };
-GLfloat  light0Dif[4] =  { 1.0, 0.6, 0.2, 1.0 };   
-GLfloat  light0Spec[4] = { 0.0, 0.0, 0.0, 1.0 };   
-GLfloat  light0Pos[4] =  { 0.0, 0.0, 0.0, 1.0 };
-GLfloat  light1Amb[4] =  { 0.0, 0.0, 0.0, 1.0 };
-GLfloat  light1Dif[4] =  { 1.0, 1.0, 1.0, 1.0 };   
-GLfloat  light1Spec[4] = { 1.0, 1.0, 1.0, 1.0 };   
-GLfloat  light1Pos[4] =  { 0.0, 5.0, 5.0, 0.0 };
-
-
 
 
 void display (void);
@@ -36,10 +21,40 @@ void keyboard (unsigned char, int, int);
 void reshape (int, int);
 void idle (void);
 void explode_cube (void);
+void init(void);
 
+void init(void)
+{
+  GLfloat  light0Amb[4] =  { 1.0, 0.6, 0.2, 1.0 };
+  GLfloat  light0Dif[4] =  { 1.0, 0.6, 0.2, 1.0 };   
+  GLfloat  light0Spec[4] = { 0.0, 0.0, 0.0, 1.0 };   
+  GLfloat  light0Pos[4] =  { 0.0, 0.0, 0.0, 1.0 };
+  GLfloat  light1Amb[4] =  { 0.0, 0.0, 0.0, 1.0 };
+  GLfloat  light1Dif[4] =  { 1.0, 1.0, 1.0, 1.0 };   
+  GLfloat  light1Spec[4] = { 1.0, 1.0, 1.0, 1.0 };   
+  GLfloat  light1Pos[4] =  { 0.0, 5.0, 5.0, 0.0 };
 
+  glShadeModel(GL_SMOOTH);
+  glClearColor (0.5f, 0.5f, 0.5f, 0.0f);
+  glClearDepth (1.0f);
+  glEnable (GL_DEPTH_TEST);
+  glDepthFunc (GL_LEQUAL);
+  glEnable (GL_COLOR_MATERIAL);
+  glHint (GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
-
+  glEnable (GL_LIGHT0);
+  glEnable (GL_LIGHT1);
+  glLightfv (GL_LIGHT0, GL_AMBIENT, light0Amb);
+  glLightfv (GL_LIGHT0, GL_DIFFUSE, light0Dif);
+  glLightfv (GL_LIGHT0, GL_SPECULAR, light0Spec);
+  glLightfv (GL_LIGHT0, GL_POSITION, light0Pos);
+  glLightfv (GL_LIGHT1, GL_AMBIENT, light1Amb);
+  glLightfv (GL_LIGHT1, GL_DIFFUSE, light1Dif);
+  glLightfv (GL_LIGHT1, GL_SPECULAR, light1Spec);
+  glLightfv (GL_LIGHT1, GL_POSITION, light1Pos);
+  glLightModelf (GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+  // glEnable (GL_NORMALIZE);
+}
 
 void display (void)
 {
@@ -47,25 +62,22 @@ void display (void)
 
   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glLoadIdentity ();
-//
-// Place the camera
+  //
+  // Place the camera
   glTranslatef (0.0, 0.0, -10.0);
   glRotatef (angle, 0.0, 1.0, 0.0);
-//
-// If no explosion, draw cube
+  //
+  // If no explosion, draw cube
   if (!cube_exploded)
   {
     glEnable (GL_LIGHTING);
     glDisable (GL_LIGHT0);
     glEnable (GL_DEPTH_TEST);
-    glutSolidCube (1.0);
+    glColor3f (1.0f, 0.0f, 0.0f);
+    glutSolidCube (5.0);
   }
   glutSwapBuffers ();
 }
-
-
-
-
 
 void keyboard (unsigned char key, int x, int y)
 {
@@ -81,10 +93,6 @@ void keyboard (unsigned char key, int x, int y)
   }
 }
 
-
-
-
-
 void reshape (int w, int h)
 {
   glViewport (0.0, 0.0, (GLfloat) w, (GLfloat) h);
@@ -94,23 +102,11 @@ void reshape (int w, int h)
   glMatrixMode (GL_MODELVIEW);
 }
 
-
-
-
-
 void idle (void)
 {
   angle += 0.3;  /* Always continue to rotate the camera */
   glutPostRedisplay ();
 }
-
-
-
-
-
-
-
-
 
 void explode_cube(void)
 {
@@ -128,22 +124,17 @@ int main (int argc, char *argv[])
   glutInitWindowSize (1280, 1024);
   glutInitDisplayMode (GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
   glutCreateWindow ("Particle explosion");
+
+  //callback functions
   glutKeyboardFunc (keyboard);
   glutIdleFunc (idle);
   glutDisplayFunc (display);
   glutReshapeFunc (reshape);
-  glEnable (GL_LIGHT0);
-  glEnable (GL_LIGHT1);
-  glLightfv (GL_LIGHT0, GL_AMBIENT, light0Amb);
-  glLightfv (GL_LIGHT0, GL_DIFFUSE, light0Dif);
-  glLightfv (GL_LIGHT0, GL_SPECULAR, light0Spec);
-  glLightfv (GL_LIGHT0, GL_POSITION, light0Pos);
-  glLightfv (GL_LIGHT1, GL_AMBIENT, light1Amb);
-  glLightfv (GL_LIGHT1, GL_DIFFUSE, light1Dif);
-  glLightfv (GL_LIGHT1, GL_SPECULAR, light1Spec);
-  glLightfv (GL_LIGHT1, GL_POSITION, light1Pos);
-  glLightModelf (GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
-  glEnable (GL_NORMALIZE);
+
+  //init
+  init();
+
+  //loop
   glutMainLoop ();
   return 0;
 }
