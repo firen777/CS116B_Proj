ff73e02822fc61ab625cd4ed37fc5a0d2e8fd8bd
firen777 <hivemindac1995@gmail.com>
2018-03-21 07:52:53 -0700

    some testcase

--- Vec3Lib_test.cpp
+++ Vec3Lib_test.cpp
@@ -1,5 +1,6 @@
 #include "aclib/vec3.h"
 #include "aclib/aclib.h"
+#include "aclib/point3.h"
 
 #include <stdio.h>
 
@@ -33,6 +34,12 @@ int main(){
     printFloatln(v3.getL());
     printVec3ln(v3.getUnit());
     printVec3ln(v3.getUnitFast());
+
+    Point3f p1(1.0,2.0,3.0);
+    Point3f p2(3.0,2.0,1.0);
+
+    printVec3ln(Vec3f(p1));
+    printVec3ln(Vec3f(p1,p2));
     
 
     return 0;
